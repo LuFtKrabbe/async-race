92bd53baa1d96c2230e4d1b954af3a87bfc90af5
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-07-16 23:15:51 +0500

    feat: add header to the winners' table

--- async-race/src/components/winners-table.ts
+++ async-race/src/components/winners-table.ts
@@ -9,4 +9,17 @@ export default function createWinnersTable(): void {
   tableWrapper.append(table);
   const tableHeader = elemHTMLClassAttr('table-header')();
   table.append(tableHeader);
+
+  const ordinalNumber = elemHTMLClassAttr('header-cell')('header', 'ordinal-number');
+  ordinalNumber.innerText = '№';
+  const carImage = elemHTMLClassAttr('header-cell')('header', 'car-image');
+  carImage.innerText = 'Image';
+  const carName = elemHTMLClassAttr('header-cell')('header', 'car-name');
+  carName.innerText = 'Name';
+  const winsNumber = elemHTMLClassAttr('header-cell')('header', 'wins-number');
+  winsNumber.innerText = 'Wins';
+  const bestTime = elemHTMLClassAttr('header-cell')('header', 'best-time');
+  bestTime.innerText = 'Best time';
+
+  tableHeader.append(ordinalNumber, carImage, carName, winsNumber, bestTime);
 }
