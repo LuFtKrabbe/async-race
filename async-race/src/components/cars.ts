b78ccf8dfe2f76e0f160076306d422f79c93f5c7
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-07-23 15:50:21 +0500

    refactor: remove not-using element creation in Cars class

--- async-race/src/components/cars.ts
+++ async-race/src/components/cars.ts
@@ -4,22 +4,17 @@ import car from '../svg/car.svg';
 export default class Cars implements DataCars {
   static carsQuantity: number;
 
-  div: HTMLElement;
-
   car: SVGSVGElement;
 
   use: SVGUseElement;
 
-  constructor() {
-    this.div = document.createElement('div');
-    this.div.classList.add('div');
+  constructor(color: string) {
     this.car = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     this.car.classList.add('car');
-    this.div.append(this.car);
     this.use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
     this.use.setAttributeNS(null, 'href', `${car}#old-auto`);
     this.car.append(this.use);
-    this.car.style.fill = '#FF00FF';
+    this.car.style.fill = `${color}`;
   }
 
   createCar() {
